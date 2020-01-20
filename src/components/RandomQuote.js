1a707372da519eff5beb55d9c09c259ba7945568
Jitgitbit <rowowolf@gmail.com>
2020-01-20 17:59:41 +0100

    some change, check git

--- src/components/RandomQuote.js
+++ src/components/RandomQuote.js
@@ -5,6 +5,8 @@ export default class RandomQuote extends Component {
     return (
       <div className="randomQuote">
         <img className="images" src={this.props.src} alt="foto"/>
+        <h1>QUOTES</h1>
+        <p>some quote</p>
       </div>
     )
   }
