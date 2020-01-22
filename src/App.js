0732aa52258526b6607ded5de02d6a9b136a9a14
Jitgitbit <rowowolf@gmail.com>
2020-01-22 09:59:13 +0100

    added likeCounter

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import RandomQuote from "./components/RandomQuote.jsx";
 import Title from "./components/Title.jsx";
 import SearchQuote from "./components/SearchQuote.jsx";
 import CategoriesComponent from "./components/CategoriesComponent";
+import LikeCounter from "./components/LikeCounter";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
      <div className="App">
         <Title /*src="https://assets.chucknorris.host/img/avatar/chuck-norris.png" alt={'a'}*/ />
         <RandomQuote/>
+        <LikeCounter/>
         <SearchQuote/>
         <CategoriesComponent/>
      </div>
