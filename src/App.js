9018073c2e724290e243a1fe4bb660051dddc67f
Basruhe <sb.ruhe@gmail.com>
2020-01-24 10:05:28 +0100

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 import "./App.css";
 import RandomQuote from "./components/RandomQuote";
+import sideBar from "./components/sideBar";
 
 function App() {
   return (
     <div className="App">
-      <div className="categoryButtons">
-        <button>button1</button>
-        <button>button2</button>
-        <button>button3</button>
-        <button>button3</button>
-      </div>
+      <div className="categoryButtons"></div>
       <RandomQuote
         src="https://assets.chucknorris.host/img/avatar/chuck-norris.png"
         alt="foto"
---
+++ src/components/sideBar.js
@@ -0,0 +1,25 @@
+// import React, { Component } from "react";
+
+// export default class sideBar extends Component {
+//   state = {
+//     cat1: false,
+//     cat2: false,
+//     cat3: false,
+//     cat4: false
+//   };
+
+// getData = () => {
+//   fetch ("https://api.chucknorris.io/jokes/random?category={category}")
+//   .then (response => response.json)
+//   .then (myJson => data) // todo: save data categories.
+// }
+
+//   render() {
+//     <div>
+//       <button></button>
+//       <button></button>
+//       <button></button>
+//       <button></button>
+//     </div>;
+//   }
+// }
